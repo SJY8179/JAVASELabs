48f3fe97775b808094ecdd18fe53bbd18f9ecab2
SJ8179 <yans61199@gmail.com>
2025-08-06 13:41:37 +0900

    Account getter setter 추가

--- src/workshop/account/entity/Account.java
+++ src/workshop/account/entity/Account.java
@@ -1,5 +1,16 @@
 package workshop.account.entity;
 
 public class Account {
-
+	private String custId;
+	private String acctId;
+	private int balance;
+	public void setBalance(int newBalance) {
+		this.balance = newBalance;
+	}
+	public String getCustId() {
+		return custId;
+	}
+	public void setCustId(String custId) {
+		this.custId = custId;
+	}
 }
