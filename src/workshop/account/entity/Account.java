91cbf82cc703b11078b7cc134248d066f768806c
SJ8179 <yans61199@gmail.com>
2025-08-06 14:41:12 +0900

    .

--- src/workshop/account/entity/Account.java
+++ src/workshop/account/entity/Account.java
@@ -4,16 +4,31 @@ public class Account {
 	private String custId;
 	private String acctId;
 	private int balance;
-	//public void setBalance(int Balance) {
-		//this.balance = Balance;
-//	}
+	
 	//default constructor선언 
 	public Account() {
 		System.out.println("기본생성자 호출됨");
 	}
+	
+	//Constructor Overloading (생성자 중복정의)
+	public Account(String custId, String acctId, int balance) {
+		super();
+		this.custId = custId;
+		this.acctId = acctId;
+		this.balance = balance;
+	}
+	
+	
+	
+	//public void setBalance(int Balance) {
+			//this.balance = Balance;
+//		}
+	
 	public int getBalance() {
 		return balance;
 	}
+	
+
 	public String getCustId() {
 		return custId;
 	}
